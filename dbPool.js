9a53129397ca3ec6405439da22573295cda5e086
Zodd <zodd@Zodd-PC.home>
2024-09-11 13:09:52 +0200

    pool vs connect database commit

---
+++ dbPool.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+// connecting Database
+module.exports = mysql.createPool({
+    connectionLimit : 10,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const database = require('./dbConnection');
+const pool = require('./dbPool');
 const express = require('express')
 const app = express()
 const PORT = 8082
@@ -5,4 +7,21 @@ const PORT = 8082
 app.use(express.json())
 app.get('/',(req,res)=>{res.send("Hi")})
 
-app.listen(PORT, console.log('Server is running on port: ' + PORT));
+database.connect((err) => {
+    if (err) {
+      console.log("Database Connection Failed !!!", err);
+    } else {
+      console.log("1 connected to Database");
+    }
+});
+
+pool.getConnection((err, connection) => {
+    if (err) {
+      console.log("Database Connection Failed !!!", err);
+    } else {
+      console.log("10 pool to Database");
+      connection.release();
+      app.listen(PORT, console.log('Server is running on port: ' + PORT));
+    }
+});
+
